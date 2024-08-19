c584176e4cf8e78146213e8d32a6b01db22a4fbe
sobuj mia <mdsobujmia287@gmail.com>
2024-08-19 22:42:01 +0300

    update

--- index.js
+++ index.js
@@ -3,19 +3,13 @@ const user_create = document.getElementById('user_create');
 
 
 
-user_create.onsubmit = (even) =>{
-    even.preventDefault()
-    let name = document.getElementById('name');
-    let contact = document.getElementById('contact');
-    let email = document.getElementById('email');
-    
+user_create.onsubmit = (e) =>{
+    e.preventDefault();
 
-     if (name.value == '' || contact.value == '' || email.value == '' ) {
-          console.log('must filap');
-          
-     }else{
-        console.log('stable data');
-        
-     }
-    
+     let form_data = new FormData(e.target);
+
+     const data = Object.fromEntries(form_data.entries())
+     console.log(data);
+     
+   
 }
